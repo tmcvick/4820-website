9b155a1bd40e56e8482866ac7dc415d6bf93b37b
Tim McVicker <tmcvick@g.clemson.edu>
2017-02-19 23:30:56 -0500

    new php

--- student_php/create_student.php
+++ student_php/create_student.php
@@ -6,14 +6,12 @@
   $created = $_REQUEST['accountcreated'];
   $username = $_REQUEST['username'];
   $gender = $_REQUEST['gender'];
-  
-  $sql = "INSERT into Student (Name, Major, AccountCreated, Username, GenderID) VALUES ('$name', '$major', '$created', '$username', '$gender')";
+
+  $sql = "INSERT into Student (Name, Major, AccountCreated, Username, GenderID) VALUES ('$name', '$major', $created, '$username', $gender)";
 
 
   if($result = mysqli_query($conn, $sql)) {
-
     echo "Student Created!";
-    header("Refresh:0; url=create_student.php");
   } else {
     echo "$conn->error";
     echo "Student creation failed";
