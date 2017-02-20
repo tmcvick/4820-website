5b272667338274db8275f868870278d94c3b1377
Tim McVicker <tmcvick@g.clemson.edu>
2017-02-19 23:08:28 -0500

    create_student php

---
+++ student_php/create_student.php
@@ -0,0 +1,19 @@
+<?php
+  include_once "include.php";
+
+  $name = $_REQUEST['name'];
+  $major = $_REQUEST['major'];
+  $created = $_REQUEST['accountcreated'];
+  $username = $_REQUEST['username'];
+  $gender = $_REQUEST['gender'];
+
+  $sql = "INSERT into STUDENT (Name, Major, AccountCreated, Username, GenderID) VALUES ('$name', '$major', '$created', '$username', '$gender')";
+
+  if($conn->query($sql) == TRUE) {
+    echo "Student Created!";
+    header("Refresh:0; url=create_student.php");
+  } else {
+    echo "Student creation failed";
+  }
+
+ ?>
