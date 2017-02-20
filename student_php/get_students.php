9604fad52e9d960f28b7968037c70d861b268e29
Tim McVicker <tmcvick@g.clemson.edu>
2017-02-20 10:42:37 -0500

    new php

---
+++ student_php/get_students.php
@@ -0,0 +1,16 @@
+<?php
+  include_once "include.php";
+
+//Gender: 1 for male, 2 for female, 3 for other
+  $sql = "Select * from Student"
+
+  if($result = mysqli_query($conn, $sql)) {
+    $resultArray = array();
+    $tempArray = array();
+    while($row = $result->fetch_object()) {
+      $tempArray = $row;
+      array_push($resultArray, $tempArray);
+    }
+    echo json_encode($resultArray);
+  }
+ ?>
