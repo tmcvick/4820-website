a907019f8622897b6a34f708b4b16d4db48b0914
Tim McVicker <tmcvick@g.clemson.edu>
2017-03-12 20:01:31 -0400

    server php files

--- todolist/getitems.php
+++ todolist/getitems.php
@@ -9,4 +9,22 @@
 include_once "include.php";
 
 $sql = "SELECT * FROM Item";
+$r = mysqli_query($conn,$sql);
+
+$res = mysqli_fetch_array($r);
+
+$result = array();
+
+array_push($result,array(
+        "id"=>$res['id'],
+        "title"=>$res['title'],
+        "priority"=>$res['priority'],
+        "duedate"=>$res['due_date'],
+        "updated"=>$res['updated_at'],
+        "deleted"=>$res['deleted_ind']
+    )
+);
+
+echo json_encode(array("result"=>$result));
+
 ?>
